296a388bf14c0088a290ddfac77617bdc5fa8378
yusica09 <144703345+yusica09@users.noreply.github.com>
2024-01-25 22:40:18 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        int b = a.nextInt();
+        Scanner c = new Scanner(System.in);
+        int d = a.nextInt();
+        System.out.println(b+d);
+    }
+}
