3ab4c98435e7b94a41beaf5db40e0c4ebbee82bc
yusica09 <144703345+yusica09@users.noreply.github.com>
2025-01-02 10:31:15 +0900

    [level 0] Title: 더 크게 합치기, Time: 3.08 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        String c = String.valueOf(a);
+        String d = String.valueOf(b);
+        
+        int ans1 = Integer.parseInt(c+d);
+        int ans2 = Integer.parseInt(d+c);
+        
+        answer = Math.max(ans1, ans2);
+        
+        return answer;
+    }
+}
